0cb5f105f3775cdcb205bd537b11678c26f08e56
Aboli Dake <35489860+aboli12@users.noreply.github.com>
2024-12-25 09:15:44 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    console.log("I'm a member of the team");
+    console.log("I have the following skills:");
+    console.log("1. Java");
+    console.log("2. JavaScript");
+    console.log("3. React");
+    console.log("4. Angular");
+}
